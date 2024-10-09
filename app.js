484cb09902f1df09fcf70c13cca06d2166345ccd
Maria Carolina <carola_sousa18@hotmail.com>
2024-10-09 15:52:17 -0300

    alterando o limite para 100

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();//aq temos quechamar a função para que ela exiba o texto.
